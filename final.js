5021497f702e6be239bdd22535177c75e9e18cfc
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-26 08:59:14 +0600

    solve

--- final.js
+++ final.js
@@ -27,3 +27,24 @@ return activeUsers;
 
 }
 
+function countHashtags(caption) {
+    if(typeof caption !== "string"){
+        return "Invalid"
+    }
+    let words=caption.split(" ");
+    let hashtagCount=0;
+    let longestTag="";
+    
+    for(let word of words){
+        if(word.startsWith("#")){
+            hashtagCount++;
+            let tag=word.slice(1);
+            if(tag.length>longestTag.length){
+                longestTag=tag;
+            }
+        }
+    }
+    return {hashtagCount:hashtagCount,longestTag:longestTag}
+}
+
+
--- pro-3.js
+++ pro-3.js
@@ -1,3 +1,20 @@
 function countHashtags(caption) {
-    // Write your code here…
+    if(typeof caption !== "string"){
+        return "Invalid"
+    }
+    let words=caption.split(" ");
+    let hashtagCount=0;
+    let longestTag="";
+    
+    for(let word of words){
+        if(word.startsWith("#")){
+            hashtagCount++;
+            let tag=word.slice(1);
+            if(tag.length>longestTag.length){
+                longestTag=tag;
+            }
+        }
+    }
+    return {hashtagCount:hashtagCount,longestTag:longestTag}
 }
+console.log(countHashtags("Loving this weather today #sunny #vibes #weekend"))
